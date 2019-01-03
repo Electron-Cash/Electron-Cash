81beb632339425115166e2a0cdaa3a48b0a7719c
Mark Lundeberg <>
2019-01-02 22:58:05 -0800

    fix tooltip

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -1110,7 +1110,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         hbox = QHBoxLayout()
         hbox.addWidget(self.message_opreturn_e)
         self.opreturn_rawhex_cb = QCheckBox(_('Hex script'))
-        self.opreturn_rawhex_cb.setToolTip(_('If unchecked, the textbox contents be UTF8-encoded and result in a single-push script: <tt>OP_RETURN PUSH &lt;text&gt;</tt>. If checked, the text contents will be interpreted as a raw hexadecimal script to be appended after the OP_RETURN opcode: <tt>OP_RETURN &lt;script&gt;</tt>.'))
+        self.opreturn_rawhex_cb.setToolTip(_('If unchecked, the textbox contents are UTF8-encoded into a single-push script: <tt>OP_RETURN PUSH &lt;text&gt;</tt>. If checked, the text contents will be interpreted as a raw hexadecimal script to be appended after the OP_RETURN opcode: <tt>OP_RETURN &lt;script&gt;</tt>.'))
         hbox.addWidget(self.opreturn_rawhex_cb)
         grid.addLayout(hbox,  3 , 1, 1, -1)
 
