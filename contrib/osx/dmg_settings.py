31707bd4d9f034c8574b6d543cb24a074d9e1376
Calin Culianu <calin.culianu@gmail.com>
2019-04-06 04:21:51 +0300

    updated dmg_settings.py to be python3.5ish

--- contrib/osx/dmg_settings.py
+++ contrib/osx/dmg_settings.py
@@ -5,8 +5,8 @@ import os.path
 PACKAGE = defines.get('PACKAGE', 'NO-PACKAGE')
 
 background = 'contrib/osx/background.png'
-volume_name = f'{PACKAGE}'
-application = f'dist/{PACKAGE}.app'
+volume_name = PACKAGE
+application = 'dist/{}.app'.format(PACKAGE)
 
 symlinks = {
     'Applications': '/Applications',
@@ -19,8 +19,8 @@ files = [
 ]
 
 icon_locations = {
-    f'{PACKAGE}.app':   (0, 140),
-    'Applications':     (230, 135),
+    '{}.app'.format(PACKAGE) :       (0, 140),
+    'Applications'           :     (230, 135),
 }
 
 window_rect = ((400, 250), (450, 300))
