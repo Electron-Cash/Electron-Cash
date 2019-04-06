be7d6e18eb1f1baecfff3fde4b3624a0e4679e6e
Calin Culianu <calin.culianu@gmail.com>
2019-04-06 04:14:49 +0300

    Added fancy dmg

---
+++ contrib/osx/dmg_settings.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os.path
+
+PACKAGE = defines.get('PACKAGE', 'NO-PACKAGE')
+
+background = 'contrib/osx/background.png'
+volume_name = f'{PACKAGE}'
+application = f'dist/{PACKAGE}.app'
+
+symlinks = {
+    'Applications': '/Applications',
+}
+
+icon = './electron.icns'
+
+files = [
+    application,
+]
+
+icon_locations = {
+    f'{PACKAGE}.app':   (0, 140),
+    'Applications':     (230, 135),
+}
+
+window_rect = ((400, 250), (450, 300))
