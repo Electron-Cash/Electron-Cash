e544cfcfd2846c991fb6e967ab3f3f9d0b74f953
Jon Spock <jonspock@protonmail.com>
2019-08-23 18:27:06 -0700

    Remove use of development team setting. Get lib path based on xcode-select

--- ios/update_project.rb
+++ ios/update_project.rb
@@ -1,24 +1,28 @@
 #!/usr/bin/ruby
 require 'xcodeproj'
-
-# This should be the Xcode ID for the team
-dev_team = "MZ11Z1Z11Z";
+require 'open3'
 
 project_path = "iOS/Electron-Cash.xcodeproj";
 
 # Create project object
 project = Xcodeproj::Project.open(project_path);
 
-lib = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/libxml2.tbd'
+stdout,stderr,status = Open3.capture3("/usr/bin/xcode-select -print-path")
+
+if status
+  lib = stdout.strip + '/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/libxml2.tbd'
+else
+  puts "Xcode not found!"
+  exit(1)
+end
+
+#puts lib
 
 project.targets.each do |target|
   build_phase = target.frameworks_build_phase
   framework_group = project.frameworks_group
   file_ref = framework_group.new_reference(lib)
   build_file = build_phase.add_file_reference(file_ref)
-  target.build_configurations.each do |config|
-    config.build_settings["DEVELOPMENT_TEAM"] =  dev_team
-  end
 end
 
 # Save the project
