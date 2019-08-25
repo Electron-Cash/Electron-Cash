5a2fa7037063679f659416190862203f98c56f8f
Jon Spock <jonspock@protonmail.com>
2019-08-25 12:48:15 -0700

    Don't run update_project.rb by default in make_ios_project.sh, update output comments

--- ios/make_ios_project.sh
+++ ios/make_ios_project.sh
@@ -251,7 +251,8 @@ fi
 # Clean up no-longer-needed electroncash/ dir that is outside of Xcode project
 rm -fr ${compact_name}/electroncash/*
 
-/usr/bin/env ruby update_project.rb
+# Can add this back when it works uniformly without issues 
+# /usr/bin/env ruby update_project.rb
 
 echo ''
 echo '**************************************************************************'
@@ -259,6 +260,14 @@ echo '*                                                                        *
 echo '*   Operation Complete. An Xcode project has been generated in "iOS/"    *'
 echo '*                                                                        *'
 echo '**************************************************************************'
+
+echo '  IMPORTANT!'
+echo '        Now you either need to manually add the library libxml2.tbd to the '
+echo '        project under "General -> Linked Frameworks and Libraries" or successfully '
+echo '        run the update_project.rb script to do it for you '
+echo '        Either of those are needed to prevent build errors! '
+echo ''
+
 echo ''
 echo '  Also note:'
 echo '        Modifications to files in iOS/ will be clobbered the next    '
