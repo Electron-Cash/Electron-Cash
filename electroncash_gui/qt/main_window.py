6a7121f404fe37de9dc92569b1e1f612ad36d281
PiRK <pierrekn@gmail.com>
2021-10-19 15:14:11 +0200

    remove dead code
    This seems to be related to a BTC TrustedCoin plugin
    https://github.com/spesmilo/electrum/commit/777a3aa8bfe751c13873a272cea2c95215c9234d
    
    There is no occurence of get_tx_extra_fee anywhere in this codebase.
    
    # Conflicts:
    #	electroncash_gui/qt/main_window.py

--- electroncash_gui/qt/main_window.py
+++ electroncash_gui/qt/main_window.py
@@ -2326,11 +2326,6 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
             _("Mining fee") + ": " + self.format_amount_and_units(fee),
         ]
 
-        x_fee = run_hook('get_tx_extra_fee', self.wallet, tx)
-        if x_fee:
-            x_fee_address, x_fee_amount = x_fee
-            msg.append( _("Additional fees") + ": " + self.format_amount_and_units(x_fee_amount) )
-
         confirm_rate = 2 * self.config.max_fee_rate()
 
         # IN THE FUTURE IF WE WANT TO APPEND SOMETHING IN THE MSG ABOUT THE FEE, CODE IS COMMENTED OUT:
