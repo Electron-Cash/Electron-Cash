2bc9d158382b9e652e8b575269a312408303c781
MrNaif2018 <chuff184@gmail.com>
2022-02-12 01:39:29 +0300

    Backport styles from electrum, enable on mac os

--- electroncash_gui/qt/__init__.py
+++ electroncash_gui/qt/__init__.py
@@ -336,13 +336,7 @@ class ElectrumGui(QObject, PrintError):
         return True
 
     def set_dark_theme_if_needed(self):
-        if sys.platform in ('darwin',):
-            # On OSX, qdarkstyle is kind of broken. We instead rely on Mojave
-            # dark mode if (built in to the OS) for this facility, which the
-            # user can set outside of this application.
-            use_dark_theme = False
-        else:
-            use_dark_theme = self.config.get('qt_gui_color_theme', 'default') == 'dark'
+        use_dark_theme = self.config.get('qt_gui_color_theme', 'default') == 'dark'
         darkstyle_ver = None
         if use_dark_theme:
             try:
@@ -355,11 +349,9 @@ class ElectrumGui(QObject, PrintError):
             except BaseException as e:
                 use_dark_theme = False
                 self.print_error('Error setting dark theme: {}'.format(repr(e)))
-        # Apply any necessary stylesheet patches. For now this only does anything
-        # if the version is < 2.6.8.
-        # 2.6.8+ seems to have fixed all the issues (for now!)
+        # Apply any necessary stylesheet patches
         from . import style_patcher
-        style_patcher.patch(dark=use_dark_theme, darkstyle_ver=darkstyle_ver)
+        style_patcher.patch(use_dark_theme=use_dark_theme, darkstyle_ver=darkstyle_ver)
         # Even if we ourselves don't set the dark theme,
         # the OS/window manager/etc might set *a dark theme*.
         # Hence, try to choose colors accordingly:
--- electroncash_gui/qt/style_patcher.py
+++ electroncash_gui/qt/style_patcher.py
@@ -5,43 +5,82 @@ It reads the current stylesheet, appends our modifications and sets the new styl
 
 from PyQt5 import QtWidgets
 from electroncash.util import print_error
+import sys
 
-def patch(dark: bool = False, darkstyle_ver: tuple = None):
-    if not dark:
-        return
+OLD_QDARKSTYLE_PATCH = '''
+QWidget:disabled {
+    color: hsl(0, 0, 50%);
+}
+QPushButton:disabled {
+    border-color: hsl(0, 0, 50%);
+    color: hsl(0, 0, 50%);
+}
+'''
 
-    app = QtWidgets.QApplication.instance()
+CUSTOM_PATCH_FOR_DARK_THEME = '''
+/* PayToEdit text was being clipped */
+QAbstractScrollArea {
+    padding: 0px;
+}
+/* In History tab, labels while edited were being clipped (Windows) */
+QAbstractItemView QLineEdit {
+    padding: 0px;
+    show-decoration-selected: 1;
+}
+/* Checked item in dropdowns have way too much height...
+   see #6281 and https://github.com/ColinDuquesnoy/QDarkStyleSheet/issues/200
+   */
+QComboBox::item:checked {
+    font-weight: bold;
+    max-height: 30px;
+}
+'''
 
+CUSTOM_PATCH_FOR_DEFAULT_THEME_MACOS = '''
+/* On macOS, main window status bar icons have ugly frame (see #6300) */
+StatusBarButton {
+    background-color: transparent;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    margin: 0px;
+    padding: 2px;
+}
+StatusBarButton:checked {
+  background-color: transparent;
+  border: 1px solid #1464A0;
+}
+StatusBarButton:checked:disabled {
+  border: 1px solid #14506E;
+}
+StatusBarButton:pressed {
+  margin: 1px;
+  background-color: transparent;
+  border: 1px solid #1464A0;
+}
+StatusBarButton:disabled {
+  border: none;
+}
+StatusBarButton:hover {
+  border: 1px solid #148CD2;
+}
+'''
+
+def patch(use_dark_theme: bool = False, darkstyle_ver: tuple = None):
+    custom_patch = ""
     if darkstyle_ver is None or darkstyle_ver < (2,6,8):
         # only apply this patch to qdarkstyle < 2.6.8.
         # 2.6.8 and above seem to not need it.
-
-        style_sheet = app.styleSheet()
-        style_sheet = style_sheet + '''
-        QWidget:disabled {
-            color: hsl(0, 0, 50%);
-        }
-        QPushButton:disabled {
-            border-color: hsl(0, 0, 50%);
-            color: hsl(0, 0, 50%);
-        }
-        '''
-        app.setStyleSheet(style_sheet)
+        custom_patch = OLD_QDARKSTYLE_PATCH
         print_error("[style_patcher] qdarkstyle < 2.6.8 detected; stylesheet patch #1 applied")
     else:
         # This patch is for qdarkstyle >= 2.6.8.
-
-        style_sheet = app.styleSheet()
-        style_sheet = style_sheet + '''
-        /* PayToEdit was being cut off on QDatkStyle >= 2.6.8. This fixes that. */
-        ButtonsTextEdit {
-            padding: 0px;
-        }
-        /* History List labels, when editing, were looking terrible in Windows and were cut off. This fixes that.*/
-        QTreeWidget QLineEdit {
-            show-decoration-selected: 1;
-            padding: 0px;
-        }
-        '''
-        app.setStyleSheet(style_sheet)
+        if use_dark_theme:
+            custom_patch = CUSTOM_PATCH_FOR_DARK_THEME
+        else:  # default theme (typically light)
+            if sys.platform == 'darwin':
+                custom_patch = CUSTOM_PATCH_FOR_DEFAULT_THEME_MACOS
         print_error("[style_patcher] qdarkstyle >= 2.6.8 detected; stylesheet patch #2 applied")
+    
+    app = QtWidgets.QApplication.instance()
+    style_sheet = app.styleSheet() + custom_patch
+    app.setStyleSheet(style_sheet)
