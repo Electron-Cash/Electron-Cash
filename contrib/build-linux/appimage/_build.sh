e08c6b5c23092b923455ecb86cda7a4f358cd7c2
Axel Gembe <axel@gembe.net>
2023-10-18 14:45:25 +0700

    Revert "Build: Fix hidapi installation and require Cython<3"
    This reverts commit 67f36cfbbd7df418f4779958efd6dec902d9a45a.

--- contrib/build-linux/appimage/_build.sh
+++ contrib/build-linux/appimage/_build.sh
@@ -119,11 +119,7 @@ mkdir -p "$CACHEDIR/pip_cache"
 CFLAGS="-g0" "$python" -m pip install --no-deps --no-warn-script-location --no-binary :all: --cache-dir "$CACHEDIR/pip_cache" -r "$CONTRIB/deterministic-build/requirements-pip.txt"
 CFLAGS="-g0" "$python" -m pip install --no-deps --no-warn-script-location --no-binary :all: --cache-dir "$CACHEDIR/pip_cache" -r "$CONTRIB/deterministic-build/requirements.txt"
 CFLAGS="-g0" "$python" -m pip install --no-deps --no-warn-script-location --no-binary :all: --only-binary pyqt5 --cache-dir "$CACHEDIR/pip_cache" -r "$CONTRIB/deterministic-build/requirements-binaries.txt"
-# Temporary fix for hidapi incompatibility with Cython 3
-# See https://github.com/trezor/cython-hidapi/issues/155
-# We use PIP_CONSTRAINT as an environment variable instead of command line flag because it gets passed to subprocesses
-# like the isolated build environment pip uses for dependencies.
-PIP_CONSTRAINT="$CONTRIB/requirements/build-constraint.txt" CFLAGS="-g0" "$python" -m pip install --no-deps --no-warn-script-location --no-binary :all: --cache-dir "$CACHEDIR/pip_cache" -r "$CONTRIB/deterministic-build/requirements-hw.txt"
+CFLAGS="-g0" "$python" -m pip install --no-deps --no-warn-script-location --no-binary :all: --cache-dir "$CACHEDIR/pip_cache" -r "$CONTRIB/deterministic-build/requirements-hw.txt"
 CFLAGS="-g0" "$python" -m pip install --no-deps --no-warn-script-location --cache-dir "$CACHEDIR/pip_cache" "$PROJECT_ROOT"
 "$python" -m pip uninstall -y -r "$CONTRIB/requirements/requirements-build-uninstall.txt"
 
